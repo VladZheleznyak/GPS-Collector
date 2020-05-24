6d27276e595750d31e8db712fdf0e3e904aec09e
VZ <vzhelezniak@fundseeder.com>
2020-05-24 14:10:07 -0400

    Yard db_wrapper

--- Gemfile
+++ Gemfile
@@ -19,6 +19,8 @@ gem 'rgeo-geojson'
 
 gem 'rubocop'
 
+gem 'yard'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- lib/db_wrapper.rb
+++ lib/db_wrapper.rb
@@ -2,21 +2,41 @@
 
 require 'pg'
 
+##
+# This class represents a DB wrapper. It connects to a DB, executes queries and converts a result if needed.
+#
+# Everything DB-related and DB-specific should be encapsulated here.
+#
+# @todo The class isn't threadsafe. At the moment the multithreading disabled by calling Rack::Lock at config.ru.
+#   To add the support, connection pooling is required
 class DbWrapper
-  def self.exec_params(sql, params_values = [])
-    # puts 'exec_params=============='
-    # puts "#{sql}"
-    # puts "#{params_values}"
+  ##
+  # Sends SQL query request specified by sql to PostgreSQL using placeholders for parameters.
+  # @param sql [String] SQL to execute
+  # @param params [Array] is an array of the bind parameters for the SQL query
+  # @return [Array] array of rows/columns returned by DB server. Should be parsed via {parse_selected_points}
+  # @example
+  #   DbWrapper.exec_params('TRUNCATE TABLE points')
+  #   => []
+  #   DbWrapper.exec_params('SELECT ST_AsText(point) FROM points WHERE ST_Distance(point, ST_GeographyFromText($1))' + \
+  #   '<= $2', ['POINT (0.01621 0.57422)', 17000000])
+  #   => [["POINT(0 0)"], ["POINT(10 0)"], ["POINT(20 0)"], ["POINT(30 0)"]]
 
+  def self.exec_params(sql, params = [])
     # TODO: (prod) credentials
     # TODO: (prod) At the moment the multithreading disabled by calling Rack::Lock at config.ru
     @conn ||= PG.connect(host: 'db', dbname: 'gps_collector', user: 'gps_collector', password: 'gps_collector')
-    # TODO: (prod) DB error processing on connect and exec
 
-    result = @conn.exec_params(sql, params_values)
+    result = @conn.exec_params(sql, params)
     result.values
   end
 
+  # Converts array of rows/columns that we got from exec_params to array of
+  # {RGeo::Cartesian::PointImpl}[https://www.rubydoc.info/gems/rgeo/RGeo/Cartesian/PointImpl]
+  #
+  # @param db_result_arr [String] Result of {exec_params}
+  # @return [Array<RGeo::Cartesian::PointImpl>] array of
+  #   {RGeo::Cartesian::PointImpl}[https://www.rubydoc.info/gems/rgeo/RGeo/Cartesian/PointImpl]
   def self.parse_selected_points(db_result_arr)
     @parser ||= RGeo::WKRep::WKTParser.new
 
